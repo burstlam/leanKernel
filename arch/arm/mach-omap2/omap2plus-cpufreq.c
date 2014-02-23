0d716002a82fc7fcbf583d380763839614c308d0
burstlam <burstlam@gmail.com>
2014-02-23 23:16:06 +0800

    Revert "Hardcode boot min frequency to 350mhz"
    This reverts commit 3d0d51adb039e7c8c60dcda03a8e9dd5303a1734.

--- arch/arm/mach-omap2/omap2plus-cpufreq.c
+++ arch/arm/mach-omap2/omap2plus-cpufreq.c
@@ -487,7 +487,7 @@ static int __cpuinit omap_cpu_init(struct cpufreq_policy *policy)
 
 	cpufreq_frequency_table_get_attr(freq_table, policy->cpu);
 
-	policy->min = 350000;
+	policy->min = policy->cpuinfo.min_freq;
 	policy->max = policy->cpuinfo.max_freq;
 	policy->cur = omap_getspeed(policy->cpu);
 
